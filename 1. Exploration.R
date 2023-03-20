999a2ab0ad5e6629845e0acd71cd4d41ebe06f2a
JeremyLvl <115343965+JeremyLvl@users.noreply.github.com>
2023-03-20 18:08:42 +0100

    Prep

---
+++ 1. Exploration.R
@@ -0,0 +1,67 @@
+# Get loan_data and view its structure
+setwd('C:/Users/jerem/Documents/GitHub/Credit risk')
+loan_data <- read.csv('loan_data.csv')
+attach(loan_data)
+str(loan_data)
+
+# Load the packages
+library(gmodels)
+library(ggplot2)
+library(tibble)
+
+#--
+
+# Check proportion of all loans default, 1 indicates default
+# We see on average 21.8% of loans default
+CrossTable(loan_status)
+
+# Check default rate by credit grade
+# As expected, the default rate is negatively correlated to the grade
+CrossTable(loan_grade, loan_status,
+           prop.c=FALSE, prop.t=FALSE, prop.chisq=FALSE)
+
+# Get a histogram of the amounts loaned
+hist_2 <- hist(loan_amnt, xlab = "Loan amount",
+               main = "Histogram of the loan amount")
+
+#--
+
+# Look at the age and income variables together
+plot(person_age, person_income, xlab = "Age", ylab = "Income")
+
+# Remove the clear outliers of age over 100
+index_highage <- which(person_age > 100)
+new_data <- loan_data[-index_highage, ]
+
+#--
+
+# Look at the interest rates
+summary(loan_int_rate)
+dim(loan_data)
+
+# We note 3116 null interest rates from 32581 data entries
+int_cat <- cut(loan_int_rate, breaks=c(5,8,11,14,17,20, Inf))
+ggplot(as_tibble(int_cat), mapping = aes(x=value)) +
+  geom_bar() + labs(x='interest rate')
+
+# We replace these with the median interest rate
+median_int <- median(loan_int_rate, na.rm=TRUE)
+na_index <- which(is.na(loan_int_rate))
+loan_data$loan_int_rate[na_index] <- median_int
+attach(loan_data)
+summary(loan_int_rate)
+
+#--
+
+# Set seed of 567
+set.seed(567)
+
+# Store row numbers for training set: index_train
+index_train <- sample(1:nrow(loan_data), 2/3*nrow(loan_data))
+
+# Create training set: training_set
+training_set <- loan_data[index_train, ]
+
+# Create test set: test_set
+test_set <- loan_data[-index_train, ]
+
